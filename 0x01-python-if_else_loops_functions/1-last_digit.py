9f774b24baccd43a643a12dab75c30a67ae821f5
Zemichael <yetikimitabeba@gmail.com>
2023-06-07 00:21:04 +0300

    pycodestyle line too long fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,15 +13,18 @@ if numlen == 1:
     elif number == 0:
         print(f"Last digit of {number} is {number} and is 0")
     else:
-        print(f"Last digit of {number} is {number} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {number} \
+                and is less than 6 and not 0")
 else:
     if number > 0:
         last_digit = (number % 10)
     else:
         last_digit = math.fmod(number, 10)
     if last_digit > 5:
-        print(f"Last digit of {number} is {last_digit:.0f} and is greater than 5")
+        print(f"Last digit of {number} is {last_digit:.0f} \
+                and is greater than 5")
     elif last_digit == 0:
         print(f"Last digit of {number} is {last_digit:.0f} and is 0")
     else:
-        print(f"Last digit of {number} is {last_digit:.0f} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last_digit:.0f} \
+                and is less than 6 and not 0")
