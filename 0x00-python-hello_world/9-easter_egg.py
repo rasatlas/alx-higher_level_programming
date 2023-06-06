18db7c5c1266f71a7ecd0717c11157037ae0678a
Zemichael <yetikimitabeba@gmail.com>
2023-06-06 03:12:47 +0300

    Task 9 - Write a Python script that prints “The Zen of Python”, by TimPeters, followed by a new line.

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 import os
-
-filename = 'zenofpython.txt'
-
-if os.path.isfile(filename):
-    with open(filename, 'r') as f:
-        print(f.read())
-else:
-    print(filename + ' does not exist.')
+fn = 'zen.txt'
+with open(fn, 'r') as f:
+    print(f.read())
