a7184b845f30903cf6ba8492885e75c302c24be7
Zemichael <yetikimitabeba@gmail.com>
2023-06-08 19:56:19 +0300

    Task 1 - Write a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print(f""+"{} + {} = {}".format(a, b, add(a, b)))
+print(f""+"{} + {} = {}".format(a, b, sub(a, b)))
+print(f""+"{} + {} = {}".format(a, b, mul(a, b)))
+print(f""+"{} + {} = {}".format(a, b, div(a, b)))
