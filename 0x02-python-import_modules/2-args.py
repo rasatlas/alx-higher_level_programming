29fc80cec06f0417c71b3f814220a9510a00801d
Zemichael <yetikimitabeba@gmail.com>
2023-06-08 21:09:23 +0300

    Task 2 - argument list count fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-result = sys.argv
-listlen = (len(result) - 1)
+
+listlen = (len(sys.argv) - 1)
+
 if listlen == 0:
     print("0 arguments.")
 elif listlen == 1:
-    print(f"{listlen} argument:")
-    print(f"{listlen}: {result[1]}")
+    print("1 argument:")
 else:
-    print(f"{listlen} arguments:")
-    for i, r in enumerate(result):
-        if i != 0:
-            print(f"{i} : {result[i]}")
+    print("{} arguments:".format(listlen))
+for i in range(listlen):
+    print("{} : {}".format(i + 1, sys.argv[i + 1]))
