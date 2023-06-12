68ef27c975859d5ff160bbdcce032c1aaa208dda
Zemichael <yetikimitabeba@gmail.com>
2023-06-12 22:49:22 +0300

    Task 8 - Write a function that returns a tuple with the length of a string and its first character.

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Write a function that returns a tuple with the length of a string
+# and its first character.
+
+def multiple_returns(sentence):
+    new_tuple = ()
+    if not sentence:
+        new_tuple = len(sentence), None
+    else:
+        new_tuple = len(sentence), sentence[0]
+        return new_tuple
