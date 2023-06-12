2d91b1158e641dd059b7d3941e5aeb831acb35c3
Zemichael <yetikimitabeba@gmail.com>
2023-06-12 21:14:31 +0300

    Task 3 - added check for existence of list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 # Write a function that prints all integers of a list, in reverse order.
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
