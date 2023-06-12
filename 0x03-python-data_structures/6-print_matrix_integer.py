cb4f95829452aad871b6ca8ec3e7ee102a324f64
Zemichael <yetikimitabeba@gmail.com>
2023-06-12 22:20:36 +0300

    Task 6 - Write a function that prints a matrix of integers.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Write a function that prints a matrix of integers.
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
