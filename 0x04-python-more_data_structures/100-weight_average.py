af4352fe554beec412b0fb261e5685c05990ec47
Zemichael <yetikimitabeba@gmail.com>
2023-06-14 22:06:18 +0300

    Task 13 - Write a function that returns the weighted average of all integers tuple (<score>, <weight>)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# a function that returns the weighted average of all
+# integers tuple (<score>, <weight>)
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    average = 0
+    div = 0
+    for tup in my_list:
+        average += tup[0] * tup[1]
+        div += tup[1]
+    return float(average / div)
