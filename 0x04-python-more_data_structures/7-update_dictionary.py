c1d8de9fb4cd985c8caf709e6bfb294108011237
Zemichael <yetikimitabeba@gmail.com>
2023-06-14 21:42:02 +0300

    Task 7 - Write a function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# a function that replaces or adds key/value in a dictionary.
+
+def update_dictionary(a_dictionary, key, value):
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for i in a_dictionary:
+            if i == key:
+                a_dictionary[i] = value
+    return a_dictionary
