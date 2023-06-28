da8eb664f2c53ee6714ec746d029798d6a94eb2a
Zemichael <yetikimitabeba@gmail.com>
2023-06-28 23:44:31 +0300

    Task 4 - Write a class Square that defines a square based on 3-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the size the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    """Calculate and return area of the square."""
+    def area(self):
+        return (self.__size * self.__size)
