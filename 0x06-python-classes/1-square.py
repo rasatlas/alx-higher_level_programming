a5dfe6a0781cb12a5c9783003d50c4cb54c68b6c
Zemichael <yetikimitabeba@gmail.com>
2023-06-28 22:27:24 +0300

    Task 1 - Write a class Square that defines a square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""An class Square that defines a square."""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
