399d14fc0f4ff261ca95cdc8ef53b97ef7b6e26d
Zemichael <yetikimitabeba@gmail.com>
2023-07-03 20:59:29 +0300

    Task 2 - Rectangle class that defines setters, getters, area and perimeter methods.

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""A class rectangle that defines a rectangle."""
+
+
+class Rectangle:
+    """Define a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Get the widht of the rectangle."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Set the width of the rectanlge."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates area of a rectangle."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Calculates perimeter of a rectangle."""
+        return ((self.__height + self.__width) * 2)
