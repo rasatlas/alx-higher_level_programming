fe5f35cbb007fbc0fde894b16a49f7bf7494a54b
Zemichael <yetikimitabeba@gmail.com>
2023-07-11 01:45:06 +0300

    Further implement Rectangle class based on 8-rectangle.py & 7-base_geometry.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines Rectangle."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent Rectangle inheritting BaseGeometry."""
+
+    def __init__(self, width, height):
+        """constructor
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Implements area method."""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectanlge] {}/{}".format(self.__width, self.__height)
