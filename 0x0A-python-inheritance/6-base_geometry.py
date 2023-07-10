7e24a2ca7b998b9da03c2ddb0f6b211bf8d7ba6c
Zemichael <yetikimitabeba@gmail.com>
2023-07-11 00:40:03 +0300

    Write a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry():
+    """Represent BaseGeometry"""
+
+    def area(self):
+        """raises Exception with specific message."""
+        raise Exception("area() is not implemented")
