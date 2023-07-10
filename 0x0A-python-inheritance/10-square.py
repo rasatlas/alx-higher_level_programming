a2133c06238a4e651f0b507bfd6f8dbc84b1ca17
Zemichael <yetikimitabeba@gmail.com>
2023-07-11 02:11:50 +0300

    Write a class Square that inherits from Rectangle (9-rectangle.py)

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,8 +14,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """Implements area for square."""
-
-        return self.__size ** 2
