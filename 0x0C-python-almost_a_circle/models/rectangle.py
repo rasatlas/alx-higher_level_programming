6ebcff35f6127281c2327863d437501e75c6e7c0
Zemichael <yetikimitabeba@gmail.com>
2023-07-18 02:09:29 +0300

    rectangle class constructor edit.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,14 +22,35 @@ class Rectangle(Base):
             ValueError: If either of x or y is < 0.
         """
         super().__init__(id)
+
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
+
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
+
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
 
     @property
     def width(self):
         """width getter."""
+
         return self.__width
 
     @width.setter
