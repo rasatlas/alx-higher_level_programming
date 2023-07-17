7a44dfb50e3149450b5fa4a06f253fb58c5d357b
Zemichael <yetikimitabeba@gmail.com>
2023-07-17 21:51:09 +0300

    Write the first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines Base class"""
+
+
+class Base():
+    """Represents Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
