04fc4c0f2155768cc9f5a3ef7dab4b96d3e23a6f
Zemichael <yetikimitabeba@gmail.com>
2023-07-18 02:23:21 +0300

    doctstring and unpacking fix.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -140,39 +140,13 @@ class Rectangle(Base):
             **kwargs (dict): New key/value pairs of attributes.
         """
 
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.width = arg
-                elif a == 2:
-                    self.height = arg
-                elif a == 3:
-                    self.x = arg
-                elif a == 4:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "width":
-                    self.width = v
-                elif k == "height":
-                    self.height = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        lst = (self.id, self.__width, self.__height, self.__x, self.__y)
+        if args:
+            self.id, self.__width, self.__height, self.__x, self.__y = \
+                    args + lst[len(args):len(lst)]
+        elif kwargs:
+            for (key, value) in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """Return the dictionary representation of a Rectangle."""
