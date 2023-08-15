9d1dcb13f582da045ff2183442842a8d697a9503
Zemichael <yetikimitabeba@gmail.com>
2023-08-16 00:22:27 +0300

    Change Characterset of a database, table and columns into utf8.

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,15 +3,6 @@
 -- Database hbtn_0c_0
 -- Table first_table
 -- Field name in first_table
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8
-COLLATE utf8_unicode_ci;
-
-ALTER first_table
-CHANGE id score name
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
