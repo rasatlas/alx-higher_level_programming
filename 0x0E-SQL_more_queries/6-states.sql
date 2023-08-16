a556cf6d64df3d2dd3d06ce248e0a0f9991414e5
Zemichael <yetikimitabeba@gmail.com>
2023-08-16 18:44:51 +0300

    A script that used auto increment and primary key constraints.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,5 +4,7 @@
 -- name VARCHAR(256) can’t be null
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
-CREATE DATABAE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE AUTO INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL);
