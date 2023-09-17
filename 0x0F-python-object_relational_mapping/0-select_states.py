00b2d14a20ad8e34fd6677754cb225c1091b40be
Zemichael <yetikimitabeba@gmail.com>
2023-09-18 02:57:22 +0300

    A script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,25 @@
- #!/usr/bin/python3
-  2 """
-  3 A script that lists all states from the database hbtn_0e_0_usa:
-  4
-  5 Your script should take 3 arguments:
-  6     mysql username, mysql password and database name
-  7     (no argument validation needed)
-  8 You must use the module MySQLdb (import MySQLdb)
-  9 Your script should connect to a MySQL server running on localhost at port 3306
- 10 Results must be sorted in ascending order by states.id
- 11 Results must be displayed as they are in the example below
- 12 Your code should not be executed when imported
- 13 """
- 14 import sys
- 15 import MySQLdb
- 16
- 17 if __name__ == '__main__':
- 18     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
- 19                          db=sys.argv[3], port=3306)
- 20
- 21     cur = db.cursor()
- 22     cur.execute("SELECT * FROM states;")
- 23     states = cur.fetchall()
- 24
- 25     for state in states:
- 26         print(state)
+#!/usr/bin/python3
+"""
+A script that lists all states from the database hbtn_0e_0_usa:
+ 
+Your script should take 3 arguments:
+mysql username, mysql password and database name
+   (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
