c43822fc5a02c7118316d2e5f8b933adb28f9632
Zemichael <yetikimitabeba@gmail.com>
2023-09-18 03:05:38 +0300

    A script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 A script that lists all states from the database hbtn_0e_0_usa:
- 
+
 Your script should take 3 arguments:
 mysql username, mysql password and database name
    (no argument validation needed)
@@ -16,7 +16,7 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3], port=3306)
+                         db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states;")
     states = cur.fetchall()
