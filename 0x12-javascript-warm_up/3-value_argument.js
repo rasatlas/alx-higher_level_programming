87197e39304bad824b6486267001fb7787704f45
Zemichael <yetikimitabeba@gmail.com>
2023-11-14 01:38:26 +0300

    A script that prints the first argument passed to it.

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,8 @@
  * A script that prints the first argument passed to it.
  */
 
-if (process.argv[2] === '') {
-  console.log('No argument');
+if (process.argv[2] != null) {
+  console.log(process.argv[2]);
 } else {
-  console.log(process.argv[3]);
+  console.log('No argument');
 }
