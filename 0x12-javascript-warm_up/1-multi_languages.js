597746ea3355d80406e2a3c2d201856040a17be5
Zemichael <yetikimitabeba@gmail.com>
2023-11-13 23:54:21 +0300

    A script that prints 3 lines.

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints 3 lines
+ */
+
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
+}
