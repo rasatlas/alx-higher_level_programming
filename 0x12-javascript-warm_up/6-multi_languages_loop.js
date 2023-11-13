14545831a43305458ec84f9027faee76c16ceba5
Zemichael <yetikimitabeba@gmail.com>
2023-11-14 02:42:47 +0300

    A script that prints 3 lines of text.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints 3 lines: (like 1-multi_languages.js) but by using
+ * an array of string and a loop
+ */
+
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
+}
