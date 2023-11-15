9b162c60ca36f62cbc249bee027d621e1920ec65
Zemichael <yetikimitabeba@gmail.com>
2023-11-16 01:29:46 +0300

    A script that imports an array and computes a new array.

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * A script that imports an array and computes a new array
+ * Your script must import list from the file 100-data.js
+ * You must use a map.
+ * A new list must be created with each value equal to the value of the-
+ * initial list, multipled by the index in the list
+ * Print both the initial list and the new list
+ */
+
+const list = require('./100-data.js');
+const newList = list.map((value, index) => value * index);
+console.log(list);
+console.log(newList);
