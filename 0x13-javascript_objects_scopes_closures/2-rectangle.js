b67009fd4fb59dc6da9250f3732a0f7c694562ec
Zemichael <yetikimitabeba@gmail.com>
2023-11-15 05:22:50 +0300

    A class that define Rectangle.

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/*
+ * class Rectangle that define a rectangle.
+ * The constructor must take 2 arguments w and h.
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ * If w or h is equal to 0 or not positive integer, create an empty object.
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || isNaN(w) || h <= 0 || isNaN(h)) {
+      const Rectangle = {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
