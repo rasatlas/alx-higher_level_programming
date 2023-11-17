297ae7c0181e53232a15b24e6ac7962bbc8e0e89
Zemichael <yetikimitabeba@gmail.com>
2023-11-17 15:05:31 +0300

    A script that lists all states from the database hbtn_0e_0_usa.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import MySQLdb
 
 """
 A script that lists all states from the databse hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
+    import MySQLdb
+
     db = MySQLdb.connect(host='localhost', port=3306,
                          user='root', passwd='root',
                          db='hbtn_0e_0_usa', charset='utf8')
