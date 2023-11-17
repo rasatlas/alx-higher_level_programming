50d1add4b91015b58ff2ff9211747867ecc6540f
Zemichael <yetikimitabeba@gmail.com>
2023-11-17 17:05:35 +0300

    A script that takes an argument and displays all the values that matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db_user = sys.argv[1]
+    db_passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    argument = sys.argv[4]
+    db_host = 'localhost'
+    db_port = 3306
+
+    db = MySQLdb.connect(host=db_host, port=db_port, user=db_user,
+                         passwd=db_passwd, db=db_name, charset='utf8')
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+                ORDER BY id ASC".format(argument))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
