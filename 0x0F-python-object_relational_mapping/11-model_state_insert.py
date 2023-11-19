b8c8cbb798dc545a719e07cb5bb78263974434d1
Zemichael <yetikimitabeba@gmail.com>
2023-11-20 00:10:50 +0300

    A script that adds the State object “Louisiana” to the database.

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -27,9 +27,9 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    new_state = State(name = 'Louisiana')
-    
+
+    new_state = State(name='Louisiana')
+
     session.add(new_state)
     session.commit()
 
