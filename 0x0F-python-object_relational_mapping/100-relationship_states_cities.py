8d9167b8cd4f35bfe96902319f1309b5f2b7fa4b
Zemichael <yetikimitabeba@gmail.com>
2023-11-20 04:24:26 +0300

    City relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+- A script that creates the State “California” with the City
+  “San Francisco” from the database hbtn_0e_100_usa
+- Your script should take 3 arguments: mysql username, mysql password and
+  database name
+- You must use the module SQLAlchemy
+- Your script should connect to a MySQL server running on localhost at
+  port 3306
+- You must use the cities relationship for all State objects
+- Your code should not be executed when imported
+"""
+
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+    new_state.cities.append(new_city)
+    session.add(new_city)
+    session.commit()
