4e0ec2b95d7d02542e7bf7797668c6cc523e2f59
Zemichael <yetikimitabeba@gmail.com>
2023-11-20 04:08:45 +0300

    Improved model_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+- Improve the file model_state.py, and save it as relationship_state.py
+- In addition to previous requirements, the class attribute cities must
+  represent a relationship with the class City.
+  If the State object is deleted, all linked City objects must be automatically
+  deleted.
+  Also, the reference from a City object to his State should be named state.
+- You must use the module SQLAlchemy
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+     class definition of a State and an instance Base = declarative_base()
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='delete', backref='state')
