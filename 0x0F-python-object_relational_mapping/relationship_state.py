45e1e06bb157578239fd36c746ad3bd7f54ea3a1
Zemichael <yetikimitabeba@gmail.com>
2023-11-20 04:41:56 +0300

    Improved model_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -10,6 +10,7 @@
 """
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
