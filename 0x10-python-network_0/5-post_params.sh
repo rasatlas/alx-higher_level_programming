ed6735e53f4562575500dd2abba501b16612c065
Zemichael <yetikimitabeba@gmail.com>
2023-12-01 01:26:01 +0300

    A script that sends POST request and sets data values

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a POST request & displays the body of the response
-curl -sX POST -d "email=test@gmail.com&subject: I will always be here for PLD" $1
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
