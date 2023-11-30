70e15fb62c41cc969afb39aee31e0a468f206f3a
Zemichael <yetikimitabeba@gmail.com>
2023-12-01 02:15:45 +0300

    A script that send a JSON POST request

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request to a URL
-curl -s -H "Content-Type: application/json" -d $(cat $2) $1
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" $1
