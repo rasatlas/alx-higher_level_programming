4bd401413ff4687a3a7243f5081474031b0e2ca6
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-02 16:52:26 +0300

    A python script that fetches a given url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     A Python script that fetches https://alx-intranet.hbtn.io/status
     """
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
-        body = r.read().decode('utf-8')
-        print(body)
+        body = r.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
