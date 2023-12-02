5a3e8e872427c890994fcb9a920e3cebc5c13b41
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-02 21:36:07 +0300

    Using package requests to make fetch url data

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     print("Body response:")
-    print(f"\t- type:", type(str({response.text})))
-    print(f"\t- Content: {response.text}")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- Content: {}".format(response.text))
