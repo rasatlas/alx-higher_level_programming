d81a3a9867c6746b8ca02ed28898aa434eefe986
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-02 17:48:34 +0300

    A python script that fetches a given url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
 A Python script that fetches https://alx-intranet.hbtn.io/status
+    - You must use the package urllib
+    - You are not allowed to import any packages other than urllib
+    - The body of the response must be formated (Refer README.md)
+    - You must use a with statement
 """
 
 
 import urllib.request
 
 if __name__ == "__main__":
-    """
-    A Python script that fetches https://alx-intranet.hbtn.io/status
-    """
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         body = r.read()
         print("Body response:")
