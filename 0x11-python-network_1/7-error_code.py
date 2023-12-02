ac745fd415c3951c6a9256c7e385bf240eccfcc8
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-03 00:37:20 +0300

    Error handling

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -15,10 +15,8 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        # If no error print
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
         print(response.text)
-    except HTTPError as e:
-        print("Error code: {}".format(e.status_code))
