5256b6e7f62067bb26157fd26560fff0882adf6d
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-02 20:18:27 +0300

    Error handling

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+    - You have to manage urllib.error.HTTPError exceptions and print:
+      Error code: followed by the HTTP status code
+    - You must use the packages urllib and sys
+    - You are not allowed to import other packages than urllib and sys
+    - You don’t need to check arguments passed to the script (number or type)
+    - You must use the with statement
+"""
+
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as r:
+            response = r.read().decode('utf-8')
+            print(response)
+    except HTTPError as e:
+        print('Error code: ', e.code)
+    except urllib.error as e:
+        print('Error code: ', e.code)
