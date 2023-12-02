ec07a226504e58bb5485a9b1877141a3f0269025
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-02 23:54:07 +0300

    Error handling

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+    - If the HTTP status code is greater than or equal to 400, print:
+    Error code: followed by the value of the HTTP status code
+    - You must use the packages requests and sys
+    - You are not allowed to import packages other than requests and sys
+    - You don’t need to check arguments passed to the script (number or type)
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__name__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if (response.status_code >= 400):
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
