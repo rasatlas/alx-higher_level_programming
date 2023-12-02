50e80faf19bca81b116beb674cdc18778b0c5adf
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-02 22:02:05 +0300

    Displaying the value of X-Request-Id from a response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+    - You must use the packages requests and sys
+    - You are not allowed to import other packages than requests and sys
+    - The value of this variable is different for each request
+    - You don’t need to check script arguments (number and type)
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    value = response.headers['X-Request-Id']
+    print(value)
