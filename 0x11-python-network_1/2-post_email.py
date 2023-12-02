72f8f0024e12a2af91020df619c31715295a89c9
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-02 19:16:24 +0300

    A python script that sends a POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -17,9 +17,8 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode(email)
-    data = data.encode('ascii') # Data should be bytes
-    request = urllib.request.Request(url, data)
+    email = sys.argv[2].encode('utf-8')
+    request = urllib.request.Request(url, data=email, method='POST')
     with urllib.request.urlopen(req) as r:
-        response = r.read()
+        response = r.read().decode('utf-8')
+        print(response)
