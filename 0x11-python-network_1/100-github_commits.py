644ec0fe5e8539dbec0d5369f78aeecc3ab84475
Zemichael <yetikimitabeba@gmail.com>
2023-12-03 21:54:35 +0300

    List 10 commits from a github repository

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A Python script that takes 2 arguments in order to solve this challenge.
+    - The first argument will be the repository name
+    - The second argument will be the owner name
+    - You must use the packages requests and sys
+    - You are not allowed to import packages other than requests and sys
+    - You don’t need to check arguments passed to the script (number or type)
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = requests.get(url)
+    json_data = response.json()
+    for i in range(10):
+        print("{}: {}".format(json_data[i].get("sha"),
+              json_data[i].get("commit").get("author").get("name")))
