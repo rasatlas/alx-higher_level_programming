d8e19402d16645702600d8e6ffbc9a21e3898e91
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-11 01:09:26 +0300

    Script that fetches json using jQuery API

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 let path = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 let response;
-$.get(path, function(response){
+$.get(path, function (response) {
     $("DIV#character").append(response.name)
 });
